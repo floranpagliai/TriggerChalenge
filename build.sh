97df18abe2e17e5f89cce34cec6cd1ed595c2a3e
Floran Pagliai <floran.pagliai@gmail.com>
2016-10-24 11:07:16 +0200

    Updated config

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-composer update --optimize-autoloader --prefer-dist
-php bin/console cache:clear --env=prod --no-debug
-php bin/console assetic:dump --env=prod --no-debug
-php bin/console doctrine:database:create
