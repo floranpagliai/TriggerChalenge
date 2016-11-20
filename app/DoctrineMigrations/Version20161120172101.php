b4a3350550e64738f2607980b9cf98123925b572
Floran Pagliai <floran.pagliai@gmail.com>
2016-11-20 17:36:51 +0100

    Fixed migration for postgresql

--- app/DoctrineMigrations/Version20161120172101.php
+++ app/DoctrineMigrations/Version20161120172101.php
@@ -15,9 +15,6 @@ class Version20161120172101 extends AbstractMigration
      */
     public function up(Schema $schema)
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
         $this->addSql('CREATE TABLE challenges (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE challenge_subjects (id INT AUTO_INCREMENT NOT NULL, cover_picture_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, type ENUM(\'PORTRAIT\', \'LANDSCAPE\', \'ARTISTIC\', \'NATURE\', \'ARCHITECTURE\', \'STREET\') NOT NULL COMMENT \'(DC2Type:challengesubject)\', description VARCHAR(255) NOT NULL, INDEX IDX_361B7EF7C50D86A0 (cover_picture_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE challenge_subjects ADD CONSTRAINT FK_361B7EF7C50D86A0 FOREIGN KEY (cover_picture_id) REFERENCES pictures (id)');
@@ -31,9 +28,6 @@ class Version20161120172101 extends AbstractMigration
      */
     public function down(Schema $schema)
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
         $this->addSql('ALTER TABLE posts DROP FOREIGN KEY FK_885DBAFA622EC58E');
         $this->addSql('DROP TABLE challenges');
         $this->addSql('DROP TABLE challenge_subjects');
