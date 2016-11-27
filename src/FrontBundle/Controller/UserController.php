7adc03dea17652d12522ed9fd53e6de4e408a6db
Floran Pagliai <floran.pagliai@gmail.com>
2016-11-27 17:34:29 +0100

    #5 Added profile page Resolve #5

--- src/FrontBundle/Controller/UserController.php
+++ src/FrontBundle/Controller/UserController.php
@@ -8,24 +8,28 @@
 namespace FrontBundle\Controller;
 
 use BackBundle\Entity\Post;
+use BackBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class UserController extends Controller
 {
 
-    public function profileAction($userId)
+    public function showAction($userId)
     {
         $user = $this->get('manager.user')->loadOneBy(array('publicId' => $userId));
-        if (!$user) {
+        if (!$user instanceof User) {
 
             return $this->redirect($this->generateUrl('front_homepage'));
         }
         /** @var Post[] $posts */
         $posts = $this->get('manager.post')->getByUserId($user->getId());
+        $stats['postCount'] = sizeof($posts);
 
-        return $this->render('FrontBundle:Index:index.html.twig',
+        return $this->render('FrontBundle:User:profile.html.twig',
             array(
-                'posts' => $posts
+                'user'  => $user,
+                'posts' => $posts,
+                'stats' => $stats
             )
         );
     }
