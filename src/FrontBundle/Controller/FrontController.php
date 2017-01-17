cfaa3447754ad4f253253954adf5881faf2d2a86
Floran Pagliai <floran.pagliai@gmail.com>
2017-01-17 18:19:17 +0100

    Fixed grid layout

--- src/FrontBundle/Controller/FrontController.php
+++ src/FrontBundle/Controller/FrontController.php
@@ -9,7 +9,7 @@ class FrontController extends Controller
     public function indexAction()
     {
         $ongoingFeaturedChallenges = $this->get('provider.challenge_subject')->getOngoingFeatured();
-        $posts = $this->get('manager.post')->getAll(10);
+        $posts = $this->get('manager.post')->getAll(16);
 
         return $this->render('FrontBundle:Index:index.html.twig',
             array(
